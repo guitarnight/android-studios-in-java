dc8ff0e04d1c472aac4b53c0283d1d746318a7f3
starnight531 <85226103+guitarnight@users.noreply.github.com>
2022-03-02 18:37:33 +0800

    converter to change form of service giving back

--- Retrofit_Adapter_flowable.java
+++ Retrofit_Adapter_flowable.java
@@ -1,3 +1,8 @@
+implementation("com.squareup.retrofit2:retrofit:2.9.0")
+implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+implementation('com.google.code.gson:gson:2.9.0')
+implementation 'com.google.android.material:material:1.3.0'
+
 package com.example.retrofit03;
 
 import androidx.appcompat.app.AppCompatActivity;
